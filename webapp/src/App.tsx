939cb5c5453b8cd68ffe4d8dee0d137d54c6b3d8
Daniel Errante <danoph@gmail.com>
2018-04-17 15:46:41 -0500

    hooking react up to backend phoenix api

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,35 @@
 import * as React from 'react';
+import * as request from 'superagent';
 import './App.css';
 
 import logo from './logo.svg';
 
+interface ITeam {
+  dynamics?: number
+  name: string
+  practices?: number
+  size?: number
+  team_size?: number
+}
+
 class App extends React.Component {
+  public state: {
+    teams: ITeam[]
+  };
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  public componentDidMount() {
+    request
+      .get('/api/teams')
+      .set('Accept', 'application/json')
+      .then(res => {
+        this.setState({ teams: res.body.data });
+      });
+  }
+
   public render() {
     return (
       <div className="App">
@@ -14,6 +40,10 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+      <pre>
+        Teams:
+        { this.state && this.state.teams[0].name }
+      </pre>
       </div>
     );
   }
